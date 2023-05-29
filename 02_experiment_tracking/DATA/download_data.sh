cffb6785150c28f4073e6f6fa436a0b7befda467
Leonardo Cesar <coderleonardo@gmail.com>
2023-05-29 20:05:59 -0300

    Remove files from wrong folder

--- 02_experiment_tracking/DATA/download_data.sh
+++
@@ -1,5 +0,0 @@
-wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-01.parquet
-
-wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-02.parquet
-
-wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-03.parquet
