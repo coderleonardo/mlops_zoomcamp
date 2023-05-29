d437d7d68afec72b22ceb335ad399c1df9d67347
Leonardo Cesar <coderleonardo@gmail.com>
2023-05-29 19:48:31 -0300

    Add DATA folder with the download_data.sh file

---
+++ 02_experiment_tracking/homework/DATA/download_data.sh
@@ -0,0 +1,5 @@
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet
+
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-02.parquet
+
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-03.parquet
